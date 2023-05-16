c56f8ad9b17a7f7293800f50deb6dedc0455e784
Adam Cooke <adam@adam.ac>
2023-05-16 10:50:33 +0100

    chore: add release please

--- lib/attach/version.rb
+++ lib/attach/version.rb
@@ -2,11 +2,6 @@
 
 module Attach
 
-  VERSION_FILE_ROOT = File.expand_path('../../VERSION', __dir__)
-  if File.file?(VERSION_FILE_ROOT)
-    VERSION = File.read(VERSION_FILE_ROOT).strip.sub(/\Av/, '')
-  else
-    VERSION = '0.0.0.dev'
-  end
+  VERSION = '1.1.2'
 
 end
